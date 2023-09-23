bd3b790613b47f22e269e86dc8fef0696a9cc83b
lbjornbak <69962560+lbjornbak@users.noreply.github.com>
2023-09-23 14:02:35 +0200

    Update fix_error3.c
    Tryfix basert på introduce_error. Introduce_error flipper bytes tror jeg så forsøker å flippe dem tilbake og sjekke ved hjelp av Get_bits fra common.

--- fix_error3.c
+++ fix_error3.c
@@ -1,67 +1,39 @@
-#include "common.h"
-#include "common.c"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdint.h>
 
-uint8_t *packets[5];
-int packet_sizes[5];
-int set_packets = 0;
+#include "common.h"
 
-// void fix_error() {
-int main(FILE *input)
-{
-    uint8_t *data = malloc(sizeof(uint8_t) * 100000);
-    
-    while(!feof(input)) {
-        get_bits(data, 14, 18);
-    for (int i = 0; i < 5; i++) 
-    {
-        packets[i] = NULL;
-    }
+void fix_error(uint8_t *packet) {
+    // Introduce_error bytter bits randomly?
+    int data_length = get_bits(packet, 16, 32) + 1;
+    // Backflip lol. Neida, men prøver flip tilbake
 
-    }
-    while (!feof(input))// finne ut hvordan du kan lese data
-    {
-        int data_read = fread(data, 1, 6, stdin);
-        int data_length = get_bits(data, 14, 18);
-        data_read += fread(data + 6, 1, data_length, stdin);
-        set_packets += 1;
-
-
-        if (set_packets == 5)
-        {
-            for (int i = 0; i < 5; i++) { 
-            for (int j = 0; j < 5; j++) {
-                if (packet_sizes[j] > packets[j + 1]) {
-                    // Bytt plass på pakkene og størrelsesinformasjonen
-                    uint8_t *packets= packets[j];
-                    int tmp = packets[j];
-                    packets[j] = packets[j + 1];
-                    packets[j + 1] = tmp;
-
-                    int packets = packets[j];
-                    packets[j] = packets[j + 1];
-                    packets[j + 1] = packets;
-
-                    int bytes_written = write(fileno(stdout), packets[i], packet_sizes[i]);
-                    fflush(stdout);
-                    packets[i] = NULL;
-                
-                set_packets = 0;
-                }
-            }
-        }
-            
-                // sorteringsfunksjon
-            
-            
-                //  bubblesort
-        }
+    for (int i = 0; i < data_length; i++) {
+        packet[i] = packet[i]; // Ingen endring? backflip
     }
 }
 
-// int main()
-// {
-//     fix_error(stdin);
-// }
+int main() {
+    uint8_t *data = malloc(sizeof(uint8_t) * 100000);
+
+    while (!feof(stdin)) {
+
+        printf("Read %lu headerbytes (Skal væer 6) - ", fread(data, 1, 6, stdin));
+        printf(" APID/header dest [%d]", get_bits(data, 11, 5));
+        printf(" Sequence counter [%d]", get_bits(data, 14, 18));
+        printf(" Data length [%d]", get_bits(data, 16, 32));
+        int data_length = get_bits(data, 16, 32);
+        printf(" - Read %lu databytes (Skal være %d)\n", fread(data, 1, data_length + 1, stdin), data_length + 1);
+
+        // Fix error
+        fix_error(data);
+
+        // Output 
+        fwrite(data, 1, data_length + 7, stdout);
+        fflush(stdout);
+    }
+
+    free(data);
+    return 0;
+}
